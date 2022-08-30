2614c6737c00b596d6a1d1bdc2aef7cfa09d4d92
penkhasoveg <pen.egor2002@gmail.com>
2022-08-30 15:39:52 +0300

    fix polls list remove scroll to end on submit

--- react/features/polls/components/web/PollsList.js
+++ react/features/polls/components/web/PollsList.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
@@ -14,32 +14,22 @@ const PollsList = () => {
     const polls = useSelector(state => state['features/polls'].polls);
     const pollListEndRef = useRef(null);
 
-    const scrollToBottom = () => {
-        if (pollListEndRef.current) {
-            pollListEndRef.current.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
-
-    useEffect(() => {
-        scrollToBottom();
-    }, [ polls ]);
-
     const listPolls = Object.keys(polls);
 
     return (
         <>
             {listPolls.length === 0
-                ? <div className = 'pane-content'>
+                ? <div className='pane-content'>
                     <Icon
-                        className = 'empty-pane-icon'
-                        src = { IconChatUnread } />
-                    <span className = 'empty-pane-message'>{t('polls.results.empty')}</span>
+                        className='empty-pane-icon'
+                        src={IconChatUnread} />
+                    <span className='empty-pane-message'>{t('polls.results.empty')}</span>
                 </div>
                 : listPolls.map((id, index) => (
                     <PollItem
-                        key = { id }
-                        pollId = { id }
-                        ref = { listPolls.length - 1 === index ? pollListEndRef : null } />
+                        key={id}
+                        pollId={id}
+                        ref={listPolls.length - 1 === index ? pollListEndRef : null} />
                 ))}
         </>
     );
